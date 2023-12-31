0a45b18d69d94a6c5ba74f4279d4ed8a9f9bf7af
gitviini <gvinicius105@gmail.com>
2023-12-31 12:58:45 -0300

    css

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const menu = document.querySelector('#menu')
 const menubox = document.querySelector('#menubox')
+const list = ['intro','produtos','contatos']
 
 menu.addEventListener('click',()=>{
     document.querySelector('#logo').classList.toggle('click')
     menu.classList.toggle('click')
     menubox.classList.toggle('click')
+})
+
+for(n = 0; n < list.length; n++){
+    var li = document.createElement('li')
+    document.querySelector('#nav').appendChild(li)
+    var a = document.createElement('a')
+    a.innerText = list[n]
+    a.setAttribute('href',`#${list[n]}`)
+    a.setAttribute('class','navigation')
+    li.appendChild(a)
+    if (n == 0)
+        a.style.borderRadius = '50px 0 0 50px'
+    else if(n == list.length - 1)
+        a.style.borderRadius = '0 50px 50px 0'
+    else
+        a.style.borderRadius = '0'
+}
+
+window.addEventListener('click', ()=>{
+
 })
